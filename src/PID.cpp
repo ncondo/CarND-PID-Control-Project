5e9fb5bd855101869734e21d957091f9c4262968
Nicholas Condo <nicholas.condo@gmail.com>
2017-06-07 01:23:54 -0400

    fix: Initialize all variables in Init member function

--- src/PID.cpp
+++ src/PID.cpp
@@ -8,9 +8,12 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-  Kp = Kp;
-  Ki = Ki;
-  Kd = Kd;
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
@@ -20,5 +23,5 @@ void PID::UpdateError(double cte) {
 }
 
 double PID::TotalError() {
-  return -Kp * p_error - Kd * d_error - Ki * i_error;
+  return (-Kp * p_error) - (Kd * d_error) - (Ki * i_error);
 }
