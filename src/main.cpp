a40e50dd60a3933dd7f9cace770d504de22e92db
Nicholas Condo <nicholas.condo@gmail.com>
2017-06-10 03:34:36 -0400

    feat: Further hyperparameter tuning of pid coefficients

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // Initialize the pid coefficients.
-  pid.Init(0.05, 0.002, 1.2);
+  pid.Init(0.06, 0.002, 1.1);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
